d2ae7631f587b78e83c974634db973c0d56fca2a
jeanberniervibert@outlook.com <jeanberniervibert@outlook.com>
2015-03-28 16:25:46 -0400

    .

---
+++ projects/Sporacid.Simplets.Webapp/Sporacid.Simplets.Webapp.App/Filters/LocalizationFilterAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using Sporacid.Simplets.Webapp.Tools.Threading;
+
+namespace Sporacid.Simplets.Webapp.App.Filters
+{
+    public class LocalizationFilterAttribute: Attribute,IActionFilter
+    {
+
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+        }
+
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var cultureHeader = filterContext.HttpContext.Request.Headers["AcceptLanguage"];
+            Thread.CurrentThread.ToCulture(cultureHeader != null ? cultureHeader : ConfigurationManager.AppSettings["DefaultLanguage"]);
+        }
+    }
+}
