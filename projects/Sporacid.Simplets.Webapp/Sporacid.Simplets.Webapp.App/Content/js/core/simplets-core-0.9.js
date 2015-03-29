021bef648f23a9059dfc9abb23157a6bf1e9014c
jeanberniervibert@outlook.com <jeanberniervibert@outlook.com>
2015-03-29 19:12:14 -0400

    add ENTER binding

--- projects/Sporacid.Simplets.Webapp/Sporacid.Simplets.Webapp.App/Content/js/core/simplets-core-0.9.js
+++ projects/Sporacid.Simplets.Webapp/Sporacid.Simplets.Webapp.App/Content/js/core/simplets-core-0.9.js
@@ -228,3 +228,24 @@ jQuery.fn.id = function (id) {
         return this.attr("id", id);
     }
 };
+
+// Capture ENTER key in element and trigger a function (<div id="xxx" data-bind="enterkey: functionX">)
+ko.bindingHandlers.enterkey = {
+    init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+        var allBindings = allBindingsAccessor();
+
+        $(element).on('keypress', 'input, textarea, select', function (e) {
+            var keyCode = e.which || e.keyCode;
+            if (keyCode !== 13) {
+                return true;
+            }
+
+            var target = e.target;
+            target.blur();
+
+            allBindings.enterkey.call(viewModel, viewModel, target, element);
+
+            return false;
+        });
+    }
+};
